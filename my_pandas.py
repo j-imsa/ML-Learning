7dbcb8acc28d7d28b7fe9d0bdb22162fd2ea081a
j-imsa <cse.isalehi@gmail.com>
2022-03-28 20:04:52 +0430

    add numpy to play

--- my_pandas.py
+++ my_pandas.py
@@ -1,8 +1,18 @@
 # https://pandas.pydata.org/
 # https://pandas.pydata.org/Pandas_Cheat_Sheet.pdf
+# https://pypi.org/project/pandas/
 # Latest version: 1.4.1
+# pip install pandas
+# pip install pandas==1.4.1
+# Pandas = Panel + Data ?
 
+import numpy as np
 import pandas as pd
+
+
+class MyTinyNumpy():
+    def __init__(self):
+        print(f'NumPy Current Running Version: {np.__version__}')
 
 
 class MyPandas():
@@ -10,9 +20,15 @@ class MyPandas():
         pass
 
     def show_pandas_version(self):
-        print(pd.__version__)
+        print(f'Pandas Current Running Version: {pd.__version__}')
+
+    def do_it(self):
+        df = pd.DataFrame([1, 2, 3, 'jimsa', True, np.nan])
+        print(df)
 
 
 if __name__ == '__main__':
-    obj = MyPandas()
-    obj.show_pandas_version()
+    numpy_obj = MyTinyNumpy()
+    pandas_obj = MyPandas()
+    pandas_obj.show_pandas_version()
+    pandas_obj.do_it()
