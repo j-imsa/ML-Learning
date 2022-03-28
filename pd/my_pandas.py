0be1f219c38d0d95ca4612557b3c8e78c42a2319
j-imsa <cse.isalehi@gmail.com>
2022-03-28 20:57:23 +0430

    add dataframe creation

--- pd/my_pandas.py
+++ pd/my_pandas.py
@@ -2,25 +2,41 @@ import numpy as np
 import pandas as pd
 
 
-class MyTinyNumpy():
+class MyTinyNumpy:
     def __init__(self):
-        print(f'NumPy Current Running Version: {np.__version__}')
+        self.message = f'NumPy Current Running Version: {np.__version__}'
+
+    def show_version(self):
+        print(self.message)
 
 
-class MyPandas():
+class MyPandas:
     def __init__(self):
-        pass
+        self.message = f'Pandas Current Running Version: {pd.__version__}'
 
-    def show_pandas_version(self):
-        print(f'Pandas Current Running Version: {pd.__version__}')
+    def show_version(self):
+        print(self.message)
 
-    def do_it(self):
+    def create_dataframe(self):
+        self.message = f'DataFrame Creation'
         df = pd.DataFrame([1, 2, 3, 'jimsa', True, np.nan])
+        df = pd.DataFrame([1, 2, 3, 'jimsa', True, np.nan], [np.nan, False, 'jimsa', 3, 2, 1])
+        df = pd.DataFrame([[1, 2, 3, 'jimsa', True, np.nan], [np.nan, False, 'jimsa', 3, 2, 1]])
+        df = pd.DataFrame([[1, 2, 3, 'jimsa'], [np.nan, False, 'jimsa', 3, 2, 1]])
+        df = pd.DataFrame(
+            {
+                'id': ['a', 'b', 'c'],
+                'data': ['a1', 'b1', 'c1'],
+            },
+            index=[1, 2, 3]
+        )
         print(df)
 
 
 if __name__ == '__main__':
     numpy_obj = MyTinyNumpy()
+    numpy_obj.show_version()
+
     pandas_obj = MyPandas()
-    pandas_obj.show_pandas_version()
-    pandas_obj.do_it()
+    pandas_obj.show_version()
+    pandas_obj.create_dataframe()
