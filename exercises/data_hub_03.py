6a36d70ce1df5e12d84db6e368189a47ebb2de85
j-imsa <cse.isalehi@gmail.com>
2022-03-29 22:25:24 +0430

    create dh_assignment_3

---
+++ exercises/data_hub_03.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def python_assignment_03_q1():
+    pass
+
+
+if __name__ == '__main__':
+    python_assignment_03_q1()
+    # python_assignment_03_q2()
+    # python_assignment_03_q3()
+    # python_assignment_03_q4()
+    # python_assignment_03_q5()
+    # python_assignment_03_q6()
+    # python_assignment_03_q7()
+    # python_assignment_03_q8()
+    # python_assignment_03_q9()
+    # python_assignment_03_q10()
