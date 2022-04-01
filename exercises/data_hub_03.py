0a8c5df0947e63888ddba6a773d1a3b64d6b034d
j-imsa <cse.isalehi@gmail.com>
2022-04-01 11:38:21 +0430

    problem three solved

--- exercises/data_hub_03.py
+++ exercises/data_hub_03.py
@@ -11,24 +11,42 @@ def python_assignment_03_q1(df):
 
 
 def python_assignment_03_q2(df):
-    pass
+    # print(df.head())
+    # df.columns = ['col1', 'col2', 'col3', 'col4']
+    # print(df.head())
+    # df.col1 = df.col1.astype('category')
+    # print(df.dtypes)
+    # print(df.col1)
+    # print(pd.get_dummies(df.col1))
+    # print(pd.get_dummies(['a', 'a', 'b', 'ab']))
+
+    for col in df.columns:
+        tmp = df[col]
+        for index, row in tmp.iteritems():
+            print(row)
+
+
+def python_assignment_03_q3(df):
+    print(df.tail())
+    tmp = df.iloc[::-1]
+    print(tmp.head())
 
 
 if __name__ == '__main__':
     # df = pd.read_csv('../pd/HousePrice.csv')
-    os.system(
-        'wget -c https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2020'
-        '-financial-year-provisional/Download-data/annual-enterprise-survey-2020-financial-year-provisional-size'
-        '-bands-csv.csv -O sample-data.csv')
+    # os.system(
+    #     'wget -c https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2020'
+    #     '-financial-year-provisional/Download-data/annual-enterprise-survey-2020-financial-year-provisional-size'
+    #     '-bands-csv.csv -O sample-data.csv')
     df = pd.read_csv('sample-data.csv')
     # python_assignment_03_q1(df)
 
-    df = pd.DataFrame([['aab', 'a', 'a', 'b'],
-                       ['ear', 'e', 'a', 'r'],
-                       ['bb', 'b', 'b']])
-    python_assignment_03_q2(df)
+    # df = pd.DataFrame([['aab', 'a', 'a', 'b'],
+    #                    ['ear', 'e', 'a', 'r'],
+    #                    ['bb', 'b', 'b']])
+    # python_assignment_03_q2(df)
 
-    # python_assignment_03_q3()
+    python_assignment_03_q3(df)
     # python_assignment_03_q4()
     # python_assignment_03_q5()
     # python_assignment_03_q6()
