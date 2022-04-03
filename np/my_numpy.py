05ff2d5df13f46a01b0c84d4aa24069e5c1d5f01
j-imsa <cse.isalehi@gmail.com>
2022-04-03 20:51:47 +0430

    update values

--- np/my_numpy.py
+++ np/my_numpy.py
@@ -24,3 +24,10 @@ print(np.arange(2, 18, 3).reshape(3, 2))
 print(np.arange(2, 18, 3).reshape(2, 3))
 # print(np.arange(2, 17, 3).reshape(2, 3)) #  ValueError: cannot reshape array of size 6 into shape (2,2)
 print(np.arange(2, 18, 3).reshape(2, 3, 1))
+
+# Update values
+arr[1] = 20
+print(arr)
+arr = np.arange(1, 21)
+arr[5:10] = -1
+print(arr)
