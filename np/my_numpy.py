b96b4d9ede92d3fa9fcea465990ce1fbaa42e149
j-imsa <cse.isalehi@gmail.com>
2022-04-03 20:48:13 +0430

    array creation using np

---
+++ np/my_numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# Array Creation
+arr = np.array([1, 2, 3])
+print(arr)
+print(np.array([
+    [1, 2, 3],
+    ['a', 'b', 'c'],
+    ['الف', 'ب']
+]))
+print(np.array([
+    [1, 2, 3],
+    [10, 20, 30],
+    [11, 21, ]
+]))
+print(np.array([1, 0, 0, 1], bool))
+
+# [start, end) , step
+print(np.arange(10))
+print(np.arange(2, 10))
+print(np.arange(2, 17, 3))
+
+print(np.arange(2, 18, 3).reshape(3, 2))
+print(np.arange(2, 18, 3).reshape(2, 3))
+# print(np.arange(2, 17, 3).reshape(2, 3)) #  ValueError: cannot reshape array of size 6 into shape (2,2)
+print(np.arange(2, 18, 3).reshape(2, 3, 1))
